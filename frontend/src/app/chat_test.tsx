1e3c0e8c6973edd370737152b80eb53e6d3f658c
Tanguy EDELIN <tedelin@paul-f4Br9s2.clusters.42paris.fr>
2023-12-14 16:55:44 +0100

    prisma init and working db except prisma studio

---
+++ frontend/src/app/chat_test.tsx
@@ -0,0 +1,14 @@
+// pages/WebSocketTest.js
+
+import WebSocketClient from './socket';
+
+const WebSocketTest = () => {
+  return (
+    <div>
+      <h1>WebSocket Test</h1>
+      <WebSocketClient />
+    </div>
+  );
+};
+
+export default WebSocketTest;
