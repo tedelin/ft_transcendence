d52ebd0c2e1e495a919aeea27f05d768f97afb2e
Tanguy EDELIN <tanguyed@proton.me>
2023-12-14 21:08:02 +0100

    database module update and rm global decorator

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ChatModule, PrismaModule],
+  imports: [ChatModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ backend/src/database/database.module.ts
@@ -0,0 +1,8 @@
+import { Global, Module } from '@nestjs/common';
+import { DatabaseService } from './database.service';
+
+@Module({
+	providers: [DatabaseService],
+	exports: [DatabaseService],
+})
+export class DatabaseModule {}
