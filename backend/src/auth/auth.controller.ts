f3a193e9060875665a12b920fb846f9564daebf4
Yassin MEHLIL <ymehlil@paul-f4Br5s1.clusters.42paris.fr>
2024-02-06 15:07:02 +0100

    resolve oauth

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -21,9 +21,9 @@ export class AuthController {
   async callback(@Query('code') code: string, @Res() res: Response) {
     const tokenData = await this.authService.callback(code);
     if (tokenData) {
-        res.redirect(`${process.env.HOST}/?token=${tokenData.access_token}`);
-    } else {
-        res.redirect(`${process.env.HOST}/`);
+        res.redirect(`${process.env.HOST}:3000/chat?token=${tokenData.access_token}`);
+        return;
     }
+    console.log('no token');
   }
 }
