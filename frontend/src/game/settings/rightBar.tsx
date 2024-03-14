e6c2564f6dbc3e64c986a48e4feabd379c19190c
catal mathis <42ecolepro@gmail.com>
2024-03-14 15:54:49 +0100

    paddle blanc

--- frontend/src/game/settings/rightBar.tsx
+++ frontend/src/game/settings/rightBar.tsx
@@ -1,20 +1,29 @@
-import paddle from '../../game_img/paddle.png';
+import { useContext } from 'react';
+import lightPaddle from '../../game_img/lightPaddle.svg';
+import darkPaddle from '../../game_img/darkPaddle.png';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import { ThemeContext } from '../../utils/providers/ThemeProvider';
 
-
-export function RightBar({handleSaveSettings}) {
+export function RightBar({ handleSaveSettings }) {
     const nav = useNavigate();
+    const { theme } = useContext(ThemeContext);
+
+    console.log('theme:', theme);
+    
+    const paddle = theme == 'light' ? lightPaddle : darkPaddle;
 
     return (
         <div className='RightBar'>
             <div className='list'>
                 <div className='listItem' id='listItem' onClick={() => nav('/game/settings/ball')} >
-					<span className="material-symbols-outlined">sports_volleyball</span>
+                    <span className="material-symbols-outlined ballLogo">sports_volleyball</span>
                     <span>Ball</span>
                 </div>
                 <div className='listItem' onClick={() => nav('/game/settings/paddle')}>
-                    <img src={paddle} alt="paddle" />
+                    {/* <span className="material-symbols-outlined"> */}
+                        <img src={paddle} alt="paddle" className='paddleLogo' />
+                    {/* </span> */}
                     <span>Paddle</span>
                 </div>
             </div>
